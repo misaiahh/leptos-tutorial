1da046b3ac0593d04193e3883f0163819bd5ae80
Micky Hallabrin <misaiahh@proton.me>
2024-02-19 23:15:01 -0500

    completed module 3.3

--- src/part_one/c_components_and_props.rs
+++ src/part_one/c_components_and_props.rs
@@ -1,21 +1,38 @@
 use leptos::*;
 
+// #[allow(non_snake_case)]
+// #[component]
+// fn ProgressBar<F: Fn() -> i32 + 'static>(
+//     // #[prop(optional)]
+//     #[prop(default = 100)] max: u16,
+//     // progress: ReadSignal<i32>,
+//     progress: F,
+// ) -> impl IntoView
+// // where
+// //     F: Fn() -> i32 + 'static,
+// {
+//     view! { <progress max=max value=progress></progress> }
+// }
+
 #[allow(non_snake_case)]
 #[component]
 fn ProgressBar(
-    progress: ReadSignal<i32>,
+    #[prop(default = 100)] max: u16,
+    #[prop(into)] progress: Signal<i32>,
 ) -> impl IntoView {
-    view! { <progress max="50" value=progress></progress> }
+    view! { <progress max=max value=progress></progress> }
 }
 
 #[allow(non_snake_case)]
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
     view! {
         <button on:click=move |_| {
             set_count.update(|n| *n += 1);
         }>"Click me"</button>
         <ProgressBar progress=count/>
+        <ProgressBar progress=Signal::derive(double_count)/>
     }
 }
