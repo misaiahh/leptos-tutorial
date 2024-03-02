3ca0a4c52af9f7be8829eb20f19e99b387203a12
Micky Hallabrin <misaiahh@proton.me>
2024-03-02 11:54:33 -0500

    completed 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use leptos::*;
 mod part_one;
 // use part_one::c_components_and_props::app;
-use crate::part_one::c_components_and_props::App;
+// use crate::part_one::c_components_and_props::App;
+use crate::part_one::e_iterating_over_more_complex_data::App;
 
 fn main() {
     _ = console_log::init_with_level(
