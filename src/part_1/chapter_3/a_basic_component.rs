a1d004147ab22326bdf2c37afc6c0f105399df42
Micky Hallabrin <misaiahh@proton.me>
2024-03-02 22:34:41 -0500

    completed 3.8 reformatted older files

--- src/part_1/chapter_3/a_basic_component.rs
+++ src/part_1/chapter_3/a_basic_component.rs
@@ -2,11 +2,11 @@ use leptos::*;
 
 #[component]
 pub fn app() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+  let (count, set_count) = create_signal(0);
 
-    view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>"Click me: " {count}</button>
-    }
+  view! {
+      <button on:click=move |_| {
+          set_count.update(|n| *n += 1);
+      }>"Click me: " {count}</button>
+  }
 }
--- src/part_1/chapter_3/components_and_props.rs
+++ src/part_1/chapter_3/components_and_props.rs
@@ -17,23 +17,23 @@ use leptos::*;
 #[allow(non_snake_case)]
 #[component]
 fn ProgressBar(
-    #[prop(default = 100)] max: u16,
-    #[prop(into)] progress: Signal<i32>,
+  #[prop(default = 100)] max: u16,
+  #[prop(into)] progress: Signal<i32>,
 ) -> impl IntoView {
-    view! { <progress max=max value=progress></progress> }
+  view! { <progress max=max value=progress></progress> }
 }
 
 #[allow(non_snake_case)]
 #[component]
 pub fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let double_count =
-        Signal::derive(move || count() * 2);
-    view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>"Click me"</button>
-        <ProgressBar progress=count/>
-        <ProgressBar progress=double_count/>
-    }
+  let (count, set_count) = create_signal(0);
+  let double_count =
+    Signal::derive(move || count() * 2);
+  view! {
+      <button on:click=move |_| {
+          set_count.update(|n| *n += 1);
+      }>"Click me"</button>
+      <ProgressBar progress=count/>
+      <ProgressBar progress=double_count/>
+  }
 }
